6fc97e097437dabe5a12df24bfca5e3f84fcfdcb
ignatov <ignatov.fedor@gmail.com>
2020-02-04 11:33:27 +0300

    feat: simple Kalman filter

--- algo.cpp
+++ algo.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <iostream>
 #include <cmath>
+#include <Eigen/Dense>
 
+#include "kalman.hpp"
 #include "params.hpp"
 
 int main ()
@@ -11,11 +13,40 @@ int main ()
     zmq::socket_t socket (context, ZMQ_REP);
 
     socket.bind ("tcp://*:5555");
+    bool first_time = true;
+    
+	int n = 3; // Number of states
+	int m = 1; // Number of measurements
+
+	double dt = 1.0/30; // Time step
+
+	Eigen::MatrixXd A(n, n); // System dynamics matrix
+	Eigen::MatrixXd C(m, n); // Output matrix
+	Eigen::MatrixXd Q(n, n); // Process noise covariance
+	Eigen::MatrixXd R(m, m); // Measurement noise covariance
+	Eigen::MatrixXd P(n, n); // Estimate error covariance
+	A << 1, dt, 0, 0, 1, dt, 0, 0, 1;
+	C << 1, 0, 0;
+	Q << .05, .05, .0, .05, .05, .0, .0, .0, .0;
+	R << 5;
+	P << .1, .1, .1, .1, 10000, 10, .1, 10, 100;
+	KalmanFilter kf(dt, A, C, Q, R, P);
+	Eigen::VectorXd y(m);
+    
     while (true) {
         zmq::message_t reply;
         socket.recv (&reply);
         Measurement* mes = static_cast<Measurement*>(reply.data());
-		
+		std::cout << mes->x << "\t";
+		if (first_time) {
+			first_time = false;
+			Eigen::VectorXd x0(n);
+			x0 << mes->x, 0, -9.81;
+			kf.init(0.0, x0);
+		}
+		y << mes->x;
+		kf.update(y);
+		std::cout << kf.state().transpose()[0] << std::endl;
 		double dx = x_c - mes->x;
 		double dy = y_c - mes->y;
 		double dl = sqrt(dx*dx+dy*dy);
@@ -45,7 +76,7 @@ int main ()
 		Command comm;
 		comm.at = at;
 		comm.ar = ar;
-		std::cout << at<<"\t" << ar<< "\t"<< target_ax << "\t" << target_ay << std::endl;
+		//std::cout << at<<"\t" << ar<< "\t"<< target_ax << "\t" << target_ay << std::endl;
 		zmq::message_t cmd ((sizeof(struct Command)));
         memcpy (cmd.data (), &comm, sizeof(struct Command));
         socket.send (cmd);
